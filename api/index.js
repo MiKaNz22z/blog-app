b1611f41f9c9bfef17759edefde0ecaf56c24cfb
DESKTOP-8KHVSP4\ADMIN <phanlom101@gmail.com>
2024-09-26 15:46:05 +0700

    add middleware and a function

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ mongoose.connect(process.env.MONGO)
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running!');
@@ -23,3 +23,13 @@ app.listen(3000, () => {
 
 app.use('/api/user', userRoutes);
 app.use('/api/auth', authRoutes);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
+})
