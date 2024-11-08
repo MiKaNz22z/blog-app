519b76ea8d1b493da95316cd46a6483cb1262c24
Your Name <vukhacdanglinh2712@gmail.com>
2024-11-08 16:59:57 +0700

    complete create post functionally

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,23 +2,26 @@ import Post from "../models/post.model.js";
 import { errorHandle } from "../utils/error.js"
 
 export const create = async (req, res, next) => {
-    console.log(req.body);
-    if(!req.user.isAdmin) {
-        return next(errorHandle(403, "You are not allow to create post"));
+    if (!req.user.isAdmin) {
+      return next(errorHandle(403, 'You are not allowed to create a post'));
     }
-    if(!req.body.title || !req.body.content) {
-        return next(errorHandle(400, "All fields are required"));
+    if (!req.body.title || !req.body.content) {
+      return next(errorHandle(400, 'Please provide all required fields'));
     }
-    const slug = req.body.title.split(" ").join("-").toLowerCase().replace(/[^a-zA-Z0-9-]/g, "-");
+    const slug = req.body.title
+      .split(' ')
+      .join('-')
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9-]/g, '');
     const newPost = new Post({
-        ...req.body, 
-        slug, 
-        userId: req.user.id,
+      ...req.body,
+      slug,
+      userId: req.user.id,
     });
-    try{
-        const savedPost = await newPost.save();
-        res.status(201).json(savedPost);
-    } catch(err) {
-        next(err)
+    try {
+      const savedPost = await newPost.save();
+      res.status(201).json(savedPost);
+    } catch (error) {
+      next(error);
     }
-}
+  };
--- client/src/page/CreatePost.jsx
+++ client/src/page/CreatePost.jsx
@@ -6,13 +6,17 @@ import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/
 import { app } from "../firebase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import 'react-circular-progressbar/dist/styles.css';
+import { useNavigate } from "react-router-dom";
 
 function CreatePost() {
   const [file, setFile] = useState(null);
   const [imageUploadProgress, setImageUploadProgress] = useState(null);
   const [imageUploadError, setImageUploadError] = useState(null);
   const [formData, setFormData] = useState({})
+  const [publlishError, setPublishError] = useState(null);
 
+  const nagivate = useNavigate();
+  
   const handleUploadImage = () => {
     try {
       if(!file) {
@@ -49,13 +53,47 @@ function CreatePost() {
     }
   }
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/post/create', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formData),
+      })
+      const data = await res.json();
+      if(!res.ok) {
+        setPublishError(data.message);
+        return;
+      }
+      if(res.ok) {
+        setPublishError(null);
+        nagivate(`/post/${data.slug}`);
+      }
+    } catch (error) {
+      setPublishError("Something went wrong. Please try again later");
+    }
+  }
+
   return (
     <div className="p-3 max-w-3xl mx-auto min-h-screen">
       <h1 className="text-center text-3xl my-7 font-semibold">Create a post</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <div className="flex flex-col gap-4 sm:flex-row justify-between">
-            <TextInput type="text" placeholder="Title" required id="title" className="flex-1"/>
-            <Select>
+            <TextInput 
+              type="text" 
+              placeholder="Title" 
+              required id="title" 
+              className="flex-1" 
+              onChange={(e) => {
+                setFormData({...formData, title: e.target.value})
+              }}
+            />
+            <Select  
+              onChange={(e) => {
+                setFormData({...formData, category: e.target.value})
+              }}
+            >
                 <option value='uncategorized'>Select a category</option>
                 <option value='javascript'>Javascript</option>
                 <option value='nextJS'>NextJS</option>
@@ -75,8 +113,15 @@ function CreatePost() {
         </div>
         { imageUploadError && <Alert color='failure'>{imageUploadError}</Alert> }
         {formData.image && <img src={formData.image} alt="upload" className="w-full h-72 object-cover"/>}
-        <ReactQuill theme="snow" placeholder="Write something..." className="h-72 mb-12" required/>
+        <ReactQuill 
+          required
+          theme="snow" 
+          placeholder="Write something..." 
+          className="h-72 mb-12" 
+          onChange={(value) => setFormData({...formData, content: value})}
+        />
         <Button type='submit' gradientDuoTone='pinkToOrange'>Publish</Button>
+        {publlishError && <Alert className="mt-5" color='failure'>{publlishError}</Alert>}
       </form>
     </div>
   )
