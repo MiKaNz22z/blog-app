5c4d1a3b7379cc9694b34565f86416ffb264d9fc
Your Name <vukhacdanglinh2712@gmail.com>
2024-12-20 16:04:29 +0700

    Complete about and projects pages

--- client/src/page/About.jsx
+++ client/src/page/About.jsx
@@ -1,7 +1,19 @@
 export default function About() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font font-semibold text-center my-7">Chào mừng đến với Zakuro's Blog</h1>
+
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>Blog web là nền tảng trực tuyến cho phép người dùng chia sẻ thông tin, suy nghĩ, kinh nghiệm và ý tưởng dưới dạng các bài viết. Đây là không gian để kết nối với cộng đồng và xây dựng các cuộc trò chuyện xung quanh các chủ đề yêu thích.</p>
+
+            <p>Với một giao diện người dùng thân thiện, blog web giúp người viết và độc giả dễ dàng tìm thấy nội dung, chia sẻ bài viết và tương tác qua phần bình luận. Các bài viết có thể được tìm kiếm theo từ khóa, chủ đề hoặc danh mục, mang đến sự tiện lợi cho người dùng.</p>
+
+            <p>Chức năng tích hợp mạng xã hội cho phép bạn chia sẻ bài viết của mình lên các nền tảng như Facebook, Twitter, hoặc Instagram, giúp tiếp cận nhiều độc giả hơn. Blog web không chỉ là nơi để chia sẻ kiến thức mà còn là công cụ xây dựng thương hiệu và phát triển kinh doanh trực tuyến.</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
